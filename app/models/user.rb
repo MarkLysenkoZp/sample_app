6046026875d141b762914295b3bdab3e0caa4994
MarkLysenkoZp <marcuslysenko061@gmail.com>
2025-06-04 17:46:15 +0300

    Password recovery

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,9 @@ class User < ActiveRecord::Base
   scope :search_by_name, ->(query) {
     where("LOWER(name) LIKE ?", "%#{query.downcase}%") if query.present?
   }
-
+    
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -34,4 +33,24 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+   def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(
+      reset_digest: User.digest(reset_token),
+      reset_sent_at: Time.zone.now
+    )
+  end
+
+  # Универсальный метод проверки токена по атрибуту
+def authenticated?(token)
+  return false if reset_digest.nil?
+  BCrypt::Password.new(reset_digest).is_password?(token)
 end
+
+
+  # Проверка, что ссылка для сброса пароля устарела
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+end 
